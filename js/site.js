d11901355fa0e7836a12aac05fb84615026519c3
Ishowor Sapkota <sapkota.ishowor@outlook.com>
2024-12-15 20:04:05 +0545

    added enquiry form

--- js/site.js
+++ js/site.js
@@ -1,10 +1,127 @@
 "use strict";
 
+var showHideObject = function(obj, show){
+	if(show){
+		$(obj).removeClass('hidden');
+	}
+	else{
+		$(obj).addClass('hidden');
+	}
+ }
+
+ var EnquiryData = function(){         
+	return {
+		EnquiryType : "",
+		SelectedNumber: "",
+		NumberOfAttendees: 0,
+		EnquiryDate: "",
+		ContactFirstName: "",
+		ContactLastName: "",
+		ContactCompanyName: "",
+		ContactNumber: "",
+		ContactEmail: "",
+		ContactAddress: "",
+		ContactPostalCode: "",
+		KeepMeUpdated: false
+	}
+}
+
+var verifySubmit = function (data){
+	 // Display collected data (can replace with AJAX logic)
+	 alert(
+	  `Enquiry Type: ${data.EnquiryType}\n` +
+	  `Selected Numbers: ${data.SelectedNumbers}\n` +
+	  (data.ContactCompanyName ? `Company Name: ${data.ContactCompanyName}\n` : `First Name: ${data.ContactFirstName}\nLast Name: ${data.ContactLastName}\n`) +
+	  `Number of Attendees: ${data.NumberOfAttendees}\n` +
+	  `Suitable Date: ${data.EnquiryDate}\n` +
+	  `Contact Number: ${data.ContactNumber}\n` +
+	  `Contact Email: ${data.ContactEmail}\n` +
+	  `Address: ${data.ContactAddress}\nPostcode: ${data.ContactPostalCode}\n` +
+	  `Marketing News: ${data.KeepMeUpdated}`
+	);
+}
+
+var collectData = function(){
+	var data = new EnquiryData();        
+
+	data.SelectedNumbers = $('input[name="selectnumbers"]:checked').map(function () {
+	return this.value;
+	}).get().join(', ');
+
+	data.EnquiryType = $('input[name="enquiryType"]:checked').val();
+
+	data.ContactFirstName = $('#firstName').val();
+	data.ContactLastName = $('#lastName').val();
+	data.ContactCompanyName = $('#companyName').val();
+	data.NumberOfAttendees = $('#attendees').val();
+	data.EnquiryDate = $('#suitableDate').val();
+	data.ContactNumber = $('#contactNumber').val();
+	data.ContactEmail = $('#contactEmail').val();
+	data.ContactAddress = $('#address').val();
+	data.ContactPostalCode = $('#postcode').val();
+	data.KeepMeUpdated = $('#marketingNews').is(':checked');
+
+	return data;
+}
+
 $(document).ready(function () {
 	/* Video Lightbox */
 	if (!!$.prototype.simpleLightboxVideo) {
 		$('.video').simpleLightboxVideo();
 	}
+
+	// Show and hide popup
+	$('#enquire').click(function () {
+		$('#overlay, #popup').fadeIn();
+	});
+
+	// Hide popup when clicking on the close button or overlay
+	$('#overlay, .close-btn').click(function () {
+		$('#overlay, #popup').fadeOut();
+	});
+
+
+	// enquiryType check
+	var callEnquiryTypeChange = function (isOrganizationChecked) {			
+		showHideObject('#nameFields',!isOrganizationChecked);
+        showHideObject('#companyField',isOrganizationChecked);
+		$('#companyName').attr('required', isOrganizationChecked); 
+		$('#firstName, #lastName').attr('required', !isOrganizationChecked);
+      };	  
+
+	  //Enquiry Type
+
+	  //checked change for enquiry type 
+	  $('#organization').change(function(){
+		var isindividualChecked = $('#individual').is(':checked');
+		if(isindividualChecked){
+			$('#individual').attr('checked', false);
+			callEnquiryTypeChange(true);
+		}		
+	  });
+
+	  $('#individual').change(function(){
+		var isOrganizationChecked = $('#organization').is(':checked');
+		if(isOrganizationChecked){
+			$('#organization').attr('checked', false);
+			callEnquiryTypeChange(false);
+		}
+		
+	  });
+
+	  //default check set to individual
+	  $('#individual').attr('checked', true);
+
+	  $('#enquiryForm').submit(function (e) {
+        e.preventDefault();
+        var inputData  = collectData();
+        verifySubmit(inputData);       
+
+        // Close popup
+        $('#overlay, #popup').fadeOut();
+      });
+
+	  
 
 	/*ScrollUp*/
 	if (!!$.prototype.scrollUp) {
